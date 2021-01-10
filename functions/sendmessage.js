f9261019d71746c53a1c48e1ee7873b74423b230
Justin Awrey <awreyjustin@gmail.com>
2021-01-10 11:47:11 -0800

    send a real message

--- functions/sendmessage.js
+++ functions/sendmessage.js
@@ -1,6 +1,17 @@
+const Pusher = require('pusher');
+
 exports.handler = async function () {
+  const pusher = new Pusher({
+    appId: process.env.PUSHER_APP_ID,
+    key: process.env.PUSHER_APP_KEY,
+    secret: process.env.PUSHER_APP_SECRET,
+  });
+
+  pusher.trigger("my-channel", "my-event", {
+    message: "A dope message."
+  });
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" })
   };
 }
