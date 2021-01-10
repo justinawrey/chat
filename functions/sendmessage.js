e0f00cb124e6f83258242a3a7442d43067b6f8e7
Justin Awrey <awreyjustin@gmail.com>
2021-01-10 13:32:52 -0800

    synchronously send pusher messages in serverless function

--- functions/sendmessage.js
+++ functions/sendmessage.js
@@ -1,21 +1,32 @@
 const Pusher = require('pusher');
 
+const pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.PUSHER_APP_KEY,
+  secret: process.env.PUSHER_APP_SECRET,
+  cluster: process.env.PUSHER_APP_CLUSTER,
+});
+
 exports.handler = async function (event) {
   const { message, socketId } = JSON.parse(event.body);
 
-  const pusher = new Pusher({
-    appId: process.env.PUSHER_APP_ID,
-    key: process.env.PUSHER_APP_KEY,
-    secret: process.env.PUSHER_APP_SECRET,
-    cluster: process.env.PUSHER_APP_CLUSTER,
-  });
+  try {
+    const pusherRes = await pusher.trigger("my-channel", "my-event", {
+      message,
+    }, socketId);
 
-  pusher.trigger("my-channel", "my-event", {
-    message,
-  }, socketId);
+    if (pusherRes.status !== 200) {
+      throw Error('Something went wrong');
+    }
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Success' }),
-  };
+    return {
+      statusCode: pusherRes.status,
+      body: 'Success',
+    }
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: err.message,
+    }
+  }
 };
