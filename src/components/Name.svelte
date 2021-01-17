e7688bdb8c06bd1b1a7700526e2616cf54dc38c7
Justin Awrey <awreyjustin@gmail.com>
2021-01-17 11:32:13 -0800

    add stuff I missed

---
+++ src/components/Name.svelte
@@ -0,0 +1,27 @@
+<script>
+  import name from "../stores/name.js";
+  let value = "";
+
+  function chooseName(e) {
+    if (e.key === "Enter" && value) {
+      $name = value;
+      value = "";
+    }
+  }
+</script>
+
+<div id="name-input">
+  <p>Name: {$name}</p>
+  <input
+    type="text"
+    placeholder="Choose name"
+    bind:value
+    on:keypress={chooseName}
+  />
+</div>
+
+<style>
+  p {
+    margin: 0 0 0.5rem 0;
+  }
+</style>
